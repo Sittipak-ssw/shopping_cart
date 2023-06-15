a7d309073e39699d7eb40c225c72ebcfac35175e
sittipak.sri <118656032+Sittipak-ssw@users.noreply.github.com>
2023-06-15 21:42:02 +0700

    Add controllers

---
+++ application/views/footer.php
@@ -0,0 +1,101 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start  text-muted">
+
+    <!-- Section: Links  -->
+    <section style="padding-top: 20px">
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-6 col-xl-3  mb-4">
+                    <!-- Content -->
+                    <img src="<?php echo base_url('assets/img/Bachira.png') ?>" alt="LOGO-S-FIT-REHAB"
+                        style="width: 120px;">
+                    <p class="mt-4">
+                        Bachira is an online store dealing with all product
+                        Bachira provide cheap, high quality, products to
+                        customers
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2  mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase mb-4 mx-auto">
+                        For Beginner
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset" style="text-decoration: none;">About</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset" style="text-decoration: none;">Career</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset" style="text-decoration: none;">Promotion</a>
+                    </p>
+
+                </div>
+
+                <div class="col-md-3 col-lg-2 col-xl-2  mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase mb-4 mx-auto">
+                        Overview
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset" style="text-decoration: none;">Product</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset" style="text-decoration: none;">Categories</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset" style="text-decoration: none;">Pricing</a>
+                    </p>
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase mb-4 mx-auto">
+                        Explore Us
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset" style="text-decoration: none;">Our Career</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset" style="text-decoration: none;">Privacy</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset" style="text-decoration: none;">Terms & Conditions</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset" style="text-decoration: none;">Help</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto">
+                    <!-- Links -->
+                    <h6 class="text-uppercase mb-4 mx-auto">Connect Us</h6>
+                    <p>support@bachira.com</p>
+                    <p>021 - 555 -456</p>
+                    <p>Sudirman, South Jakarta</p>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4">
+        Copyright 2023 • All Rights Reserved Bachira by Giatinaja
+
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
